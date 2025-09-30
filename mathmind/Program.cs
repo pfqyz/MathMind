f55977407879d325c1d79852b15b84553d4bcf42
Софья Зайцева <apelsinsofa@gmail.com>
2025-09-30 20:28:21 +0300

    Добавление проекта

---
+++ mathmind/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MathMind
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"2 + 2 = {2 + 2}");
+        }
+    }
+}
